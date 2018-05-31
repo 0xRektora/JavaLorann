52ff037e552be4cb8588a7a8bb88a89ed7ccdc97
Bloodyline <bloodyline01@gmail.com>
2018-05-31 16:31:29 +0100

    Some improvement of the collision and pawns systems.

--- ExiaEngine/src/main/java/org/ExiaEngine/BoardPanel.java
+++ ExiaEngine/src/main/java/org/ExiaEngine/BoardPanel.java
@@ -14,28 +14,30 @@ public class BoardPanel extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	/** The keyboard controller */
 	private KeyboardControl kbControll;
-	
 	Player lorann = new Player();
 	
-	BoardPanel(){
+
+	BoardPanel() {
 		super();
 		this.setFocusable(true);
-	    this.requestFocusInWindow();
+		this.requestFocusInWindow();
 		this.setBackground(Color.BLACK);
 		this.kbControll = new KeyboardControl(this.lorann);
-		this.addKeyListener(kbControll);
+		this.addKeyListener(this.kbControll);
+		Enemies enemy = new Enemies(64, 64);
 	}
-	
+
 	@Override
-    protected void paintComponent(Graphics g) {
-    	 	super.paintComponent(g);
-    	    g.drawImage(this.lorann.getSprite(), this.lorann.getX(), this.lorann.getY(), this);
-    	    if(!this.lorann.hasSpell()) {
-    	    	g.drawImage(this.lorann.getSpell().getSprite(), this.lorann.getSpell().getX(), this.lorann.getSpell().getY(), this);
-    	    }
-    	  }
-	
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		if (Pawn.getPawns().size() > 0) {
+			for (Pawn i : Pawn.getPawns()) {
+				g.drawImage(i.getSprite(), i.getX(), i.getY(), this);
+			}
+		}
+	}
+
 }
--- ExiaEngine/src/main/java/org/ExiaEngine/KeyboardControl.java
+++ ExiaEngine/src/main/java/org/ExiaEngine/KeyboardControl.java
@@ -4,66 +4,69 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import MotionElement.Pawn;
+import MotionElement.Player;
 
 public class KeyboardControl implements KeyListener {
-	
+
 	/** The pawn we'll move. */
 	private Pawn pawn;
-	
+
 	/**
 	 * The constructor
+	 * 
 	 * @param pawn
-	 * 		take the pawn we wanna move.
+	 *            take the pawn we wanna move.
 	 */
 	public KeyboardControl(Pawn pawn) {
 		this.pawn = pawn;
 	}
 
-	
 	/**
 	 * Function to listen the pawn controller and move it around the map.
+	 * 
 	 * @param e
 	 */
 	@Override
 	public void keyPressed(KeyEvent e) {
-		
-		switch(e.getKeyCode()) {
-		case KeyEvent.VK_RIGHT:
-			this.pawn.move_right();
-			break;
-		case KeyEvent.VK_LEFT:
-			this.pawn.move_left();
-			break;
-		case KeyEvent.VK_UP:
-			this.pawn.move_up();
-			break;
-		case KeyEvent.VK_DOWN:
-			this.pawn.move_down();
-			break;
-		case KeyEvent.VK_SPACE:
-			this.pawn.shoot();
-			break;
-		default:
-			break;
-		
+		if (this.pawn.isAlive()) {
+			switch (e.getKeyCode()) {
+			case KeyEvent.VK_RIGHT:
+				this.pawn.move_right();
+				break;
+			case KeyEvent.VK_LEFT:
+				this.pawn.move_left();
+				break;
+			case KeyEvent.VK_UP:
+				this.pawn.move_up();
+				break;
+			case KeyEvent.VK_DOWN:
+				this.pawn.move_down();
+				break;
+			case KeyEvent.VK_SPACE:
+				this.pawn.shoot();
+				break;
+			case KeyEvent.VK_ENTER:
+				System.out.println("enter");
+				Player player = new Player();
+				break;
+			default:
+				break;
+
+			}
 		}
 
 	}
 
-
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-
-
 
 }
